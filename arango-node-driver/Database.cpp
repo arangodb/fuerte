925495622bf5af5073fa3be84e0e578097a72e9a
Jan Christoph Uhde <Jan@UhdeJC.com>
2016-11-02 15:24:57 +0100

    continue wrapping

--- arango-node-driver/Database.cpp
+++ arango-node-driver/Database.cpp
@@ -4,61 +4,33 @@
 #include "Database.h"
 #include "Server.h"
 
-namespace arangodb {
+namespace arangodb { namespace dbnodejs {
 
-namespace dbnodejs {
-
-Nan::Persistent<v8::Function> Server::_constructor;
-
-Database::Database(std::shared_ptr<arangodb::dbinterface::Server> const& server, const std::string name)
-  : _cppDatabase(std::make_shared<arangodb::dbinterface::Server>(server, name))
-  {}
-
-
-  NAN_MODULE_INIT(Server::Init) {
-  Nan::HandleScope scope;
-  // Prepare constructor template
-  // Create Javascript new Server object template
-  v8::Local<v8::FunctionTemplate> tpl = Nan::New<v8::FunctionTemplate>(New);
-  tpl->SetClassName(Nan::New("Database").ToLocalChecked());
-
-  // Only 1 internal field required for this wrapped class
-  tpl->InstanceTemplate()->SetInternalFieldCount(1);
-
-  // TODO Register prototypes for Javascript functions
-  //
-  //Nan::SetPrototypeMethod(tpl, "makeConnection", Server::makeConnection);
-  //Nan::SetPrototypeMethod(tpl, "version", Server::version);
-
-  // Provides Javascript with GetVersion constructor
-  // and destructor?
-  _constructor.Reset(tpl->GetFunction());
-  target->Set(Nan::New("Database").ToLocalChecked(), tpl->GetFunction());
-}
-
-NAN_METHOD(Server::New) {
+NAN_METHOD(Database::New) {
   if (info.IsConstructCall()) {
-    Server* obj = nullptr;
 
     // warum plain pointer wer loescht?
     int nArgs = info.Length();
-    if (nArgs > 1) {
-      Nan::ThrowTypeError("Too many arguments");
+    if (nArgs !=2 ) {
+      Nan::ThrowTypeError("Not 2 Arguments");
     }
-    if (!nArgs) {
-      // Default Server url http://127.0.0.1:8529
-      obj = new Server();
-    }
+
     v8::Local<v8::Value> val = info[0];
     if (!val->IsString()) {
       Nan::ThrowTypeError("String parameter required");
     }
-    v8::String::Utf8Value tmp{val};
-    std::string inp{*tmp};
-    obj = new Server(inp);
+
+    if (info[0]->IsObject() && info[1]->IsString()){
+
+      Nan::ObjectWrap::Unwrap<Server>(info[0]);
+
+      //find out how to convert
+      Nan::To<double>(info[1]);
+      Database* obj = new Database(info[0], Nan::To<v8::String>(info[1]));
 
     obj->Wrap(info.This());
     info.GetReturnValue().Set(info.This());
+  }
   } else {
     // Invoked as plain function `GetVersion(...)`,
     // turn into construct call.
@@ -71,13 +43,11 @@ NAN_METHOD(Server::New) {
     for (int i = 0; i < argc; ++i) {
       argv[i] = info[i];
     }
-    v8::Local<v8::Function> cons = Nan::New<v8::Function>(_constructor);
+    v8::Local<v8::Function> cons = Nan::New(constructor());
     auto val = cons->NewInstance(argc, argv);
-    info.GetReturnValue().Set(val);
+    info.GetReturnValue().Set(Nan::NewInstance(cons,argc,argv).ToLocalChecked());
   }
 }
 
 
-}
-}
-
+}}
--- arango-node-driver/Database.h
+++ arango-node-driver/Database.h
@@ -5,25 +5,46 @@
 #include <fuerte/Database.h>
 #include <nan.h>
 
-namespace arangodb {
-
-namespace dbnodejs {
+namespace arangodb { namespace dbnodejs {
 
 class Database : public Nan::ObjectWrap {
  public:
-  static NAN_MODULE_INIT(Init);
+  // see https://github.com/nodejs/nan/blob/master/doc/object_wrappers.md#api_nan_object_wrap
+  // for explanations
+  static NAN_MODULE_INIT(Server::Init) {
+    v8::Local<v8::FunctionTemplate> tpl = Nan::New<v8::FunctionTemplate>(New);
+    tpl->SetClassName(Nan::New("Database").ToLocalChecked());
+
+    // Only 1 internal field required for this wrapped class ( _cppDatabase )
+    tpl->InstanceTemplate()->SetInternalFieldCount(1);
+
+    //Nan::SetPrototypeMethod(tpl, "makeConnection", Server::makeConnection);
+    //Nan::SetPrototypeMethod(tpl, "version", Server::version);
+
+    constructor().Reset(Nan::GetFunction(tpl).toLocalChecked());
+
+    target->Set( Nan::New("Database").ToLocalChecked()
+               , Nan::GetFunction(tpl).toLocalChecked()
+               );
+  }
+
   static NAN_METHOD(New);
-  static NAN_METHOD(create);
+  //static NAN_METHOD(create);
 
  private:
-  Database::Database(std::shared_ptr<arangodb::dbinterface::Server> const& server, const std::string name);
-  //static Server* Create(const Nan::FunctionCallbackInfo<v8::Value>& info);
+  Database::Database(std::shared_ptr<arangodb::dbinterface::Server> const& server, const std::string name)
+    : _cppDatabase(std::make_shared<arangodb::dbinterface::Server>(server, name))
+    {}
 
+  static Nan::Persistent<v8::Function>& constructor(){
+    static Nan::Persistent<v8::Function> ctor;
+    return ctor;
+  }
+
+  //only field in this class
   std::shared_ptr<arangodb::dbinterface::Server> _cppDatabase;
-  static Nan::Persistent<v8::Function> _constructor;
 };
-}
-}
 
+}}
 #endif  // FUERTE_NODESERVER_H
 
