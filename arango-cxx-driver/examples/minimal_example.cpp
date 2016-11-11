de9853e092656a3099af8fd54db6cdec53ad90c2
Jan Christoph Uhde <Jan@UhdeJC.com>
2016-11-11 14:46:52 +0100

    rename

--- arango-cxx-driver/examples/minimal_example.cpp
+++ arango-cxx-driver/examples/minimal_example.cpp
@@ -1,34 +1,12 @@
-#include <fuerte/Server.h>
-#include <fuerte/Database.h>
-#include <fuerte/Collection.h>
-#include <fuerte/Document.h>
-#include <velocypack/Slice.h>
-#include <velocypack/Builder.h>
-#include <string>
-#include <utility>
+#include <fuerte/next/arangocxx.h>
 
 int main(){
-    //init
-    using namespace arangodb::dbinterface;
-    auto server = std::make_shared<Server>("tcp://localhost:8529");
-    auto db = std::make_shared<Database>(server, "test_database");
-    auto coll = std::make_shared<Collection>(db, "my_collection");
-    auto conn = server->makeConnection();
-    conn->setAsynchronous(false);
-
-    //create data
-    arangodb::velocypack::Builder builder;
-    builder.openObject();
-    builder.add("_id",arangodb::velocypack::Value("wauuuu"));
-    builder.close();
-
-    //add document
-    Document{}.create(coll,conn,builder.steal());
-
-    //run query
-    conn->setPostReq();
-    conn->run();
-
-    //inspect result
-    Connection::VPack res = conn->result();
+    using namespace arangodb::rest;
+    auto conn = ConnectionBuilder().host("http://localhost:8529")
+//                                   .async(true)
+                                   .user("hund")
+                                   .password("arfarf")
+                                   .connect();
+    auto coll = conn->getDatabase("fopples")->getCollection("plastic");
+    coll->insert("coca cola standard fopple");
 }
