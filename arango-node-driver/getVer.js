f9ad44444bbf46dd5202116b5829a7192dd9c2fd
Jan Christoph Uhde <Jan@UhdeJC.com>
2017-02-04 13:48:31 +0100

    add more code to get version

--- arango-node-driver/getVer.js
+++ arango-node-driver/getVer.js
@@ -2,5 +2,20 @@ var fuerte = require('bindings')('arango-node-driver')
 
 // create server
 var builder = new fuerte.ConnectionBuilder()
-var con = builder.host("vst://127.0.0.1:8529").connect();
+var connection = builder.host("vst://127.0.0.1:8529").connect();
 
+var request = new fuerte.Request();
+request.setRestVerb("get");
+request.setPath("_api/version/");
+
+var onError = function(code, req, res){
+  console.log("error")
+}
+
+var onSuccess = function(req, res){
+  console.log("succes")
+}
+
+connection.sendRequest(request, onError, onSuccess);
+
+//fuerte.poll(); //expose via c++
