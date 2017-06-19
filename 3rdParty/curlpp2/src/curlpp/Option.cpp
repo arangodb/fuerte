9baf96055f1e00cbd19ddff9e7b295d049eaaf63
Ewout Prangsma <ewout@prangsma.net>
2017-06-19 10:26:01 +0200

    Removed unused curlpp2, replace with CURL

--- 3rdParty/curlpp2/src/curlpp/Option.cpp
+++
@@ -1,5 +0,0 @@
-#include "curlpp/Option.hpp"
-#include "curlpp/internal/OptionSetter.hpp"
-
-template <typename OptionType, CURLoption option>
-const CURLoption curlpp::OptionTrait<OptionType, option>::option;
