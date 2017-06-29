44a622dae44714170e5f45636a18250e8e511f9c
Ewout Prangsma <ewout@prangsma.net>
2017-06-29 14:41:51 +0200

    Added getters for ConnectionBuilder

--- include/fuerte/connection.h
+++ include/fuerte/connection.h
@@ -94,6 +94,7 @@ class Connection : public std::enable_shared_from_this<Connection> {
  */
 class ConnectionBuilder {
   public:
+    inline std::string host() const { return _conf._host; }
     ConnectionBuilder& host(std::string const&); // takes url in the form  (http|vst)[s]://(ip|hostname):port
                                                  // sets protocol host and port
     //ConnectionBuilder() = delete;
@@ -105,14 +106,19 @@ class ConnectionBuilder {
     std::shared_ptr<Connection> connect(EventLoopService& eventLoopService);
 
     // Set the authentication type of the connection
+    inline AuthenticationType authenticationType() const { return _conf._authenticationType; }
     ConnectionBuilder& authenticationType(AuthenticationType t){ _conf._authenticationType = t; return *this; }
     // Set the username of the connection
+    inline std::string user() const { return _conf._user; }
     ConnectionBuilder& user(std::string const& u){ _conf._user = u; return *this; }
     // Set the password of the connection
+    inline std::string password() const { return _conf._password; }
     ConnectionBuilder& password(std::string const& p){ _conf._password = p; return *this; }
     // Set the maximum size for chunks (VST only)
+    inline std::size_t maxChunkSize() const { return _conf._maxChunkSize; }
     ConnectionBuilder& maxChunkSize(std::size_t c){ _conf._maxChunkSize = c; return *this; }
     // Set the VST version to use (VST only)
+    inline vst::VSTVersion vstVersion() const { return _conf._vstVersion; }
     ConnectionBuilder& vstVersion(vst::VSTVersion c){ _conf._vstVersion = c; return *this; }
     // Set a callback for connection failures that are not request specific.
     ConnectionBuilder& onFailure(ConnectionFailureCallback c){ _conf._onFailure = c; return *this; }
