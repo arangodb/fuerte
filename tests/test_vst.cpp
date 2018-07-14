73d8de70c8d36160df13d29125e99f68db100d27
Simon Grätzer <simon@graetzer.org>
2018-07-14 15:45:05 +0200

    added multichunk test

--- tests/test_vst.cpp
+++ tests/test_vst.cpp
@@ -28,9 +28,9 @@ namespace fu = ::arangodb::fuerte;
 
 // testsuite for VST 1.1
 
-TEST(VSTParser, ChunkHeader) {
+TEST(VelocyStream_11, ChunkHeader) {
   std::string const length("\x1C\0\0\0", 4); // 24 + 4 bytes length
-  std::string const chunkX("\x03\0\0\0", 4); // 0 chunks
+  std::string const chunkX("\x03\0\0\0", 4); // 1 chunk
   std::string const mid("\x01\0\0\0\0\0\0\0", 8); // messageId 1
   std::string const mLength("\x04\0\0\0\0\0\0\0", 8); // messageLength
   std::string const data("\x0a\x0b\x0c\x0d", 4); // messageId 1
@@ -54,10 +54,82 @@ TEST(VSTParser, ChunkHeader) {
   ptr = reinterpret_cast<uint8_t const*>(header._data.data());
   uint32_t val = arangodb::basics::uintFromPersistentLittleEndian<uint32_t>(ptr);
   ASSERT_EQ(val, static_cast<uint32_t>(0x0d0c0b0a));
+  
+  arangodb::velocypack::Buffer<uint8_t> buffer;
+  size_t t = header.writeHeaderToVST1_1(buffer);
+  ASSERT_EQ(buffer.size(), fu::vst::maxChunkHeaderSize);
+  ASSERT_EQ(t, fu::vst::maxChunkHeaderSize);
+  ASSERT_LE(fu::vst::maxChunkHeaderSize, chunk.length());
+  ASSERT_TRUE(memcmp(buffer.data(), chunk.data(), fu::vst::maxChunkHeaderSize) == 0);
 }
 
-TEST(VSTParser, MultiChunk){
-  ASSERT_TRUE(true); //TODO -- DELETE
+TEST(VelocyStream_11, MultiChunk){
+  std::string const length("\x1C\0\0\0", 4); // 24 + 4 bytes length
+  std::string const chunkX_0("\x07\0\0\0", 4); // 3 chunks = ((0b11 << 1) | 1)
+  std::string const chunkX_1("\x02\0\0\0", 4); // chunk 1 ((0b01 << 1) | 0)
+  std::string const chunkX_2("\x04\0\0\0", 4); // chunk 2 ((0b10 << 1) | 0)
+  std::string const mid("\x01\0\0\0\0\0\0\x01", 8); // messageId
+  std::string const mLength("\x0C\0\0\0\0\0\0\0", 8); // messageLength
+  std::string const data("\x0a\x0b\x0c\x0d", 4); // messageId 1
+ 
+  std::string chunk0 = length + chunkX_0 + mid + mLength + data;
+  std::string chunk1 = length + chunkX_1 + mid + mLength + data;
+  std::string chunk2 = length + chunkX_2 + mid + mLength + data;
+
+  ASSERT_EQ(chunk0.size(), 28);
+  ASSERT_EQ(chunk1.size(), 28);
+  ASSERT_EQ(chunk2.size(), 28);
   
+  // chunk 0
+  uint8_t const* ptr = reinterpret_cast<uint8_t const*>(chunk0.c_str());
+  size_t chunkLength = fu::vst::parser::isChunkComplete(ptr, chunk0.size());
+  ASSERT_EQ(chunkLength, 28);
+  fu::vst::ChunkHeader header = fu::vst::parser::readChunkHeaderVST1_1(ptr);
+  ASSERT_EQ(header.chunkLength(), 28);
+  ASSERT_EQ(header.messageID(), (1ULL << 56ULL) + 1ULL);
+  ASSERT_EQ(header.messageLength(), 0x0C);
+  ASSERT_TRUE(header.isFirst());
+  ASSERT_EQ(header.index(), 0);
+  ASSERT_EQ(header.numberOfChunks(), 3);
+  ASSERT_EQ(header._data.size(), 4);
+  ptr = reinterpret_cast<uint8_t const*>(header._data.data());
+  uint32_t val = arangodb::basics::uintFromPersistentLittleEndian<uint32_t>(ptr);
+  ASSERT_EQ(val, static_cast<uint32_t>(0x0d0c0b0a));
   
+  // chunk 1
+  ptr = reinterpret_cast<uint8_t const*>(chunk1.c_str());
+  chunkLength = fu::vst::parser::isChunkComplete(ptr, chunk1.size());
+  ASSERT_EQ(chunkLength, 28);
+  header = fu::vst::parser::readChunkHeaderVST1_1(ptr);
+  ASSERT_EQ(header.chunkLength(), 28);
+  ASSERT_EQ(header.messageID(), (1ULL << 56ULL) + 1ULL);
+  ASSERT_EQ(header.messageLength(), 0x0C);
+  ASSERT_TRUE(!header.isFirst());
+  ASSERT_EQ(header.index(), 1);
+  ASSERT_EQ(header._data.size(), 4);
+  ptr = reinterpret_cast<uint8_t const*>(header._data.data());
+  val = arangodb::basics::uintFromPersistentLittleEndian<uint32_t>(ptr);
+  ASSERT_EQ(val, static_cast<uint32_t>(0x0d0c0b0a));
+  
+  // chunk 2
+  ptr = reinterpret_cast<uint8_t const*>(chunk2.c_str());
+  chunkLength = fu::vst::parser::isChunkComplete(ptr, chunk2.size());
+  ASSERT_EQ(chunkLength, 28);
+  header = fu::vst::parser::readChunkHeaderVST1_1(ptr);
+  ASSERT_EQ(header.chunkLength(), 28);
+  ASSERT_EQ(header.messageID(), (1ULL << 56ULL) + 1ULL);
+  ASSERT_EQ(header.messageLength(), 0x0C);
+  ASSERT_TRUE(!header.isFirst());
+  ASSERT_EQ(header.index(), 2);
+  ASSERT_EQ(header._data.size(), 4);
+  ptr = reinterpret_cast<uint8_t const*>(header._data.data());
+  val = arangodb::basics::uintFromPersistentLittleEndian<uint32_t>(ptr);
+  ASSERT_EQ(val, static_cast<uint32_t>(0x0d0c0b0a));
+  
+  /*arangodb::velocypack::Buffer<uint8_t> buffer;
+  size_t t = header.writeHeaderToVST1_1(buffer);
+  ASSERT_EQ(buffer.size(), fu::vst::maxChunkHeaderSize);
+  ASSERT_EQ(t, fu::vst::maxChunkHeaderSize);
+  ASSERT_LE(fu::vst::maxChunkHeaderSize, chunk.length());
+  ASSERT_TRUE(memcmp(buffer.data(), chunk.data(), fu::vst::maxChunkHeaderSize) == 0);*/
 }
