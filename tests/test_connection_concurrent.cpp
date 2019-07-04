68562d840c2fb45bdbe73a46b4ffae7c70030efc
Simon Grätzer <simon@graetzer.org>
2019-07-04 15:39:43 +0200

    increase test timeout

--- tests/test_connection_concurrent.cpp
+++ tests/test_connection_concurrent.cpp
@@ -130,7 +130,7 @@ TEST_P(ConcurrentConnectionF, CreateDocumentsParallel){
       }
     });
   }
-  ASSERT_TRUE(wg.wait_for(std::chrono::seconds(60))); // wait for all threads to return
+  ASSERT_TRUE(wg.wait_for(std::chrono::seconds(300))); // wait for all threads to return
 
   // wait for all threads to end
   for (std::thread& t : joins) {
